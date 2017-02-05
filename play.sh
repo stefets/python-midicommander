dfa1446eb466966ed7af51cb67b136755005a75c
Stephane Gagnon <admin@pacificweb.ca>
2017-02-05 13:18:14 -0500

    Optimisation

--- play.sh
+++ play.sh
@@ -4,9 +4,5 @@
 # Play filename on a specific audio harware
 # ADJUST AUDIODEV for your needs
 
-filename=$1
-if [ -e $filename ]
-then
-	#kill -9 $(pgrep play) 2>/dev/null
-	AUDIODEV=hw:1,0 play $1
-fi
+#export AUDIODEV=hw:1,0
+if [ -e $filename ]; then play -q $1; fi
