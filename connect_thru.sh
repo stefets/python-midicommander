312e9afc50c0c75dc3a2a71648867e52d998f557
Stephane Gagnon <stephane-gagnon@hotmail.com>
2017-04-13 16:09:39 -0400

    Ajustements

--- connect_thru.sh
+++ connect_thru.sh
@@ -5,16 +5,16 @@
 # Client 0 = SD-90 Part A
 # Client 1 = SD-90 Part B
 
-# Disconnect all
+# Disconnect Thru
 aconnect -d 14:0 20:0
 aconnect -d 14:0 20:1
 
 client=0	# Default
 
-if [[ $1 =~ ^[0-1]+$ ]]
-then
-	client=$1
-fi	
+#if [[ $1 =~ ^[0-1]+$ ]]
+#then
+#	client=$1
+#fi	
 
 aconnect 14:0 20:$client
 aconnect -l
